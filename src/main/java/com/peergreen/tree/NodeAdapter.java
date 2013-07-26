2ba8dac35e6d92d327b142d06f4250601159ecc4
Guillaume Sauthier <guillaume.sauthier@peergreen.com>
2013-07-26 13:20:38 +0200

    Use latest parent POM * Simplified POM * Adapted license headers * Specified used plugin version

--- src/main/java/com/peergreen/tree/NodeAdapter.java
+++ src/main/java/com/peergreen/tree/NodeAdapter.java
@@ -1,11 +1,6 @@
-/*
- * Copyright 2013 Peergreen S.A.S.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
+/**
+ * Copyright 2012-2013 Peergreen S.A.S. All rights reserved.
+ * Proprietary and confidential.
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
